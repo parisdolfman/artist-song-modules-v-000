2d964ffcdf498095b6ea9cfef1af94f1518f1c5f
Paris Dolfman <parisdolfman@gmail.com>
2019-09-09 19:03:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable
+  extend Findable
   attr_accessor :name
   attr_reader :artist
 
@@ -11,9 +12,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
